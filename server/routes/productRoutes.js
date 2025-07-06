ffb7d0efb308585b09251f6c2c96bba8da8465f7
Minh Triet <phoenixmt1705@gmail.com>
2025-07-06 20:21:10 +0700

    add route and controller for product and add to server

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const getProduct = require("../controllers/productController");
+
+router.route("/").get(getProduct);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("LegacyCues Server is running!");
 });
+
+app.use("/api/product", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is listenning on port: ${port}`);
