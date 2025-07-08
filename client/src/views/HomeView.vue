9d2afeae88b77a2b05e185a817bd6ca4d89f7b80
Minh Triet <phoenixmt1705@gmail.com>
2025-07-08 13:31:26 +0700

    change the page view of the vue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Đây là trang chủ của LegacyCues</h1>
   </main>
 </template>
