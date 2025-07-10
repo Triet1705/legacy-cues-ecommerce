ba20674b8d0cf689822a877207dc4649a2d0f163
Minh Triet <phoenixmt1705@gmail.com>
2025-07-10 16:36:27 +0700

    test code common button

---
+++ client/src/components/common/ButtonCommon.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+<template>
+  <button class="base-button">
+    <span class="text">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+<style scoped>
+.base-button {
+  align-items: center;
+  justify-content: center;
+  display: inline-block;
+  border: none;
+  border-radius: 5px;
+  background-color: #333;
+  color: #fff;
+  font-weight: 600;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 1rem 2.5rem;
+}
+.base-button:hover {
+  background-color: #555;
+}
+.text {
+  line-height: 1;
+}
+</style>
