0c1d02ac0089ed617b8fb0fde4c76e9c99b0620e
Minh Triet <phoenixmt1705@gmail.com>
2025-07-12 23:56:05 +0700

    function for add to cart and UI for Cart page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProductCollectionView from '../views/ProductCollectionView.vue'
 import ProductDetailView from '@/views/ProductDetailView.vue'
 import HomeView from '@/views/HomeView.vue'
+import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView,
     },
     {
       path: '/product/:id',
