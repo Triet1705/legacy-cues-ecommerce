2d7e08442797d3428cf4de953c48c41b887a7d98
Minh Triet <phoenixmt1705@gmail.com>
2025-07-25 16:34:15 +0700

    fix responsive for bigger width

--- client/src/views/ProductCollectionView.vue
+++ client/src/views/ProductCollectionView.vue
@@ -50,7 +50,7 @@ onMounted(() => {
 }
 .banner {
   margin-bottom: 2rem;
-  width: 1536px;
+  width: 100%;
   height: 420px;
   background-image: url('../assets/images/pericontact.jpg');
   background-position: center;
