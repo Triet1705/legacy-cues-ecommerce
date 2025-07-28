36351e4f8f6941607cc9c3a70f2903ff8a7f8ca8
Minh Triet <phoenixmt1705@gmail.com>
2025-07-28 17:36:40 +0700

    finish authentication middleware and add protect to api endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
 const {
   registerUser,
@@ -13,6 +14,9 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 module.exports = router;
